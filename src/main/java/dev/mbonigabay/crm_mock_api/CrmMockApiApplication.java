5f3735f2d506fe611ab2dd442fd91b138d28d1f2
mbonigabay <mbonigabay@gmail.com>
2026-02-01 11:06:22 +0200

    first commit

---
+++ src/main/java/dev/mbonigabay/crm_mock_api/CrmMockApiApplication.java
@@ -0,0 +1,13 @@
+package dev.mbonigabay.crm_mock_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmMockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmMockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mbonigabay/crm_mock_api/CrmMockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mbonigabay.crm_mock_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmMockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
